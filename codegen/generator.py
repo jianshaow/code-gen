4a1d0b81ed37104d018107ba6e11c77677de597a
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-03-21 03:40:24 +0000

    update

--- codegen/generator.py
+++ codegen/generator.py
@@ -6,21 +6,23 @@ from transformers import (
     GenerationConfig,
 )
 
-bnb_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16)
-
 
 def generate(model_name="replit/replit-code-v1_5-3b", hint="def fibonacci(n):"):
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     generation_config = GenerationConfig.from_pretrained(model_name, max_length=1024)
-    generation_config.pad_token_id = generation_config.eos_token_id
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16
+    )
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
         trust_remote_code=True,
-        quantization_config=bnb_config,
+        quantization_config=quantization_config,
         device_map="auto",
     )
+
     inputs = tokenizer.encode(hint, return_tensors="pt").to("cuda:0")
     sample = model.generate(inputs, generation_config=generation_config)
+
     return tokenizer.decode(sample[0])
 
 
