896be08355509d4048132318fc5897fcdb22f029
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-03-21 10:32:40 +0000

    update

--- codegen/generator.py
+++ codegen/generator.py
@@ -7,20 +7,29 @@ from transformers import (
 )
 
 
-def generate(model_name="replit/replit-code-v1_5-3b", hint="def fibonacci(n):"):
+def generate(
+    model_name="replit/replit-code-v1_5-3b", hint="def fibonacci(n):", bnb_quan=True
+):
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     generation_config = GenerationConfig.from_pretrained(model_name, max_length=1024)
-    quantization_config = BitsAndBytesConfig(
-        load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16
-    )
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        trust_remote_code=True,
-        quantization_config=quantization_config,
-        device_map="auto",
-    )
+    if bnb_quan:
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16
+        )
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            trust_remote_code=True,
+            quantization_config=quantization_config,
+            device_map="auto",
+        )
+    else:
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            trust_remote_code=True,
+            device_map="auto",
+        )
 
-    inputs = tokenizer.encode(hint, return_tensors="pt").to("cuda:0")
+    inputs = tokenizer.encode(hint, return_tensors="pt").cuda()
     sample = model.generate(inputs, generation_config=generation_config)
 
     return tokenizer.decode(sample[0])
@@ -29,6 +38,7 @@ def generate(model_name="replit/replit-code-v1_5-3b", hint="def fibonacci(n):"):
 if __name__ == "__main__":
     code = generate(
         # "Salesforce/codegen25-7b-multi",
+        # bnb_quan=False,
     )
     print("======================================================")
     print(code)
