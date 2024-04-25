a0163b4f5fec2f9543c7c1fe1144d45e87c7dfc1
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-04-25 09:50:23 +0000

    add default

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ import os
 
 tmpl_dir = os.environ.get("PROMPT_TMPL_DIR", "prompts")
 base_url = os.environ.get("OPENAI_API_BASE")
-api_key = os.environ.get("OPENAI_API_KEY")
+api_key = os.environ.get("OPENAI_API_KEY", "EMPTY")
 model = os.environ.get("OPENAI_MODEL_NAME", "gpt-3.5-turbo")
 
 
