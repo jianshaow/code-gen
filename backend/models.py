8a9e905ba4c32604338e3e12426b1bd064aafb22
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-04-28 14:40:23 +0000

    suppress error

--- backend/models.py
+++ backend/models.py
@@ -25,24 +25,29 @@ def get_models(reload=False):
         if config.api_spec == "openai":
             __models = [obj.id for obj in get_client().models.list().data]
         elif config.api_spec == "ollama":
-            url = "{base_url}/api/tags".format(base_url=__get_ollama_base_url())
-            response = requests.get(url)
-            if response.status_code == 200:
-                json_data = response.json()
-                __models = [obj["name"] for obj in json_data["models"]]
-            else:
-                raise HTTPError(
-                    url=url, code=response.status_code, msg="call ollama api fail"
-                )
+            base_url = __get_ollama_base_url()
+            if base_url:
+                url = "{base_url}/api/tags".format(base_url=base_url)
+                response = requests.get(url)
+                if response.status_code == 200:
+                    json_data = response.json()
+                    __models = [obj["name"] for obj in json_data["models"]]
+                else:
+                    raise HTTPError(
+                        url=url, code=response.status_code, msg="call ollama api fail"
+                    )
         else:
             raise NotImplementedError(f"not supported spec {config.api_spec}")
 
-    return __models
+    return __models != None and __models or []
 
 
 def __get_ollama_base_url():
-    parsed_url = urlsplit(config.base_url)
-    return f"{parsed_url.scheme}://{parsed_url.netloc}"
+    if config.base_url:
+        parsed_url = urlsplit(config.base_url)
+        return f"{parsed_url.scheme}://{parsed_url.netloc}"
+    else:
+        return None
 
 
 if __name__ == "__main__":
