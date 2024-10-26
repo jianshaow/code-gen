45dc15e548f5e6ea09b30721cf312fb9846e4ed8
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-10-26 16:45:43 +0800

    rename

--- build-image.sh
+++ build-image.sh
@@ -6,7 +6,7 @@ docker pull ${base_image}:latest
 tag=$(docker inspect --format='{{index .Config.Labels "version"}}' ${base_image})
 echo "Using llm api base version ${tag}"
 
-image=jianshao/codegen-demo
+image=jianshao/code-gen-demo
 version=$(date +%Y%m%d)
 docker build -t ${image}:latest . --build-arg TAG=${tag} --build-arg VERSION=${version} $*
 
