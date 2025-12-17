dadd27a3d628ea195ebbec0a7bbcc1a02bd64d06
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-12-17 09:36:05 +0000

    migrate to function components

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,138 +1,109 @@
 import 'highlight.js/styles/github.css';
-import React, { type ChangeEvent, Component, type MouseEvent } from 'react';
+import { useEffect, useRef, useState, type ChangeEvent, type MouseEvent } from 'react';
 import { Link } from 'react-router-dom';
-import { fetchApiConfig, fetchConfig, fetchTemplates, gen_stream, generate } from '../services/backend';
+import { fetchApiConfig, fetchConfig, fetchTemplates, gen_stream } from '../services/backend';
 import './Common.css';
 import './Home.css';
 import MarkdownViewer from './Markdown';
 
-interface HomeState {
-  apiSpec: string;
-  model: string;
-  templates: string[];
-  template: string;
-  requirement: string;
-  generated: string;
-  copied: boolean;
-}
+function Home() {
+  const [apiSpec, setApiSpec] = useState('');
+  const [model, setModel] = useState('');
+  const [templates, setTemplates] = useState<string[]>([]);
+  const [template, setTemplate] = useState('');
+  const [requirement, setRequirement] = useState('make an example');
+  const [generated, setGenerated] = useState('');
+  const [copied, setCopied] = useState(false);
+  const codeRef = useRef<HTMLDivElement>(null);
 
-class Home extends Component<{}, HomeState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      apiSpec: '', model: '', templates: [], template: '',
-      requirement: 'make an example', generated: '',
-      copied: false,
-    };
-  }
-
-  codeRef = React.createRef<HTMLDivElement>();
-
-  componentDidMount() {
-    this.initConfig();
-    this.initTemplate();
-  }
-
-  initConfig() {
+  function initConfig() {
     fetchConfig().then(config => {
-      this.setState({
-        apiSpec: config.api_spec,
-      });
-      fetchApiConfig(config.api_spec).then(config => {
-        this.setState({
-          model: config.model,
-        });
+      setApiSpec(config.api_spec);
+      fetchApiConfig(config.api_spec).then(config2 => {
+        setModel(config2.model);
       });
     });
   }
 
-  initTemplate() {
-    fetchTemplates(false).then(templates => {
-      this.setState({ templates: templates, template: templates[0] })
+  function initTemplate() {
+    fetchTemplates(false).then(templatesData => {
+      setTemplates(templatesData);
+      setTemplate(templatesData[0]);
     });
   }
 
-  handleGenerate = async (_e: MouseEvent) => {
-    const { template, requirement } = this.state;
-
-    generate(template, requirement).then(generated => {
-      this.setState({ generated: generated });
-    });
-  };
-
-  handleGenerateStream = async (_e: MouseEvent) => {
-    const { template, requirement } = this.state;
-
-    gen_stream(template, requirement, (generated: string) => {
-      this.setState({ generated: generated });
-      if (this.codeRef.current) {
-        this.codeRef.current.scrollTop = this.codeRef.current.scrollHeight;
+  const handleGenerateStream = async (_e: MouseEvent) => {
+    gen_stream(template, requirement, (generatedData: string) => {
+      setGenerated(generatedData);
+      if (codeRef.current) {
+        codeRef.current.scrollTop = codeRef.current.scrollHeight;
       }
     });
   };
 
-  render() {
-    const { apiSpec, model, templates, template, requirement, generated, copied } = this.state;
+  useEffect(() => {
+    initConfig();
+    initTemplate();
+  }, []);
 
-    return (
-      <div className='main-frame'>
-        <div className='header'>
-          <Link to='/setting'>Setting</Link>
+  return (
+    <div className='main-frame'>
+      <div className='header'>
+        <Link to='/setting'>Setting</Link>
+      </div>
+      <h1 className='title'>Code Generator</h1>
+      <label>Current Setting</label>
+      <div className='info-block'>
+        <div>
+          <label>API Spec</label>
+          <div className='info-value'>{apiSpec}</div>
         </div>
-        <h1 className='title'>Code Generator</h1>
-        <label>Current Setting</label>
-        <div className='info-block'>
-          <div>
-            <label>API Spec</label>
-            <div className='info-value'>{apiSpec}</div>
-          </div>
-          <div>
-            <label>Model</label>
-            <div className='info-value'>{model}</div>
-          </div>
-          <div>
-            <label>Template</label>
-            <div>
-              <select value={template} onChange={(e: ChangeEvent<HTMLSelectElement>) => {
-                this.setState({ template: e.target.value })
-              }}>{templates.map(data => (
-                <option key={data} value={data}>{data}</option>
-              ))}
-              </select>
-            </div>
-          </div>
+        <div>
+          <label>Model</label>
+          <div className='info-value'>{model}</div>
         </div>
-        <div className='container'>
-          <div className='requirement-block'>
-            <label>Requiremenet</label>
-            <textarea value={requirement} rows={30}
-              onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
-                this.setState({ requirement: e.target.value });
-              }} />
-          </div>
-          <div className='container-column'>
-            <button onClick={this.handleGenerateStream}>=&gt;</button>
-          </div>
-          <div className='generated-block'>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <label>Generated Code</label>
-              <div>
-                {copied && <label className='success'>Copied</label>}
-                <button onClick={async (_e: MouseEvent) => {
-                  navigator.clipboard.writeText(generated);
-                  this.setState({ copied: true });
-                  setTimeout(() => {
-                    this.setState({ copied: false });
-                  }, 2000);
-                }} style={{ 'textAlign': 'right' }}>Copy</button>
-              </div>
-            </div>
-            <MarkdownViewer content={generated} height={426} />
+        <div>
+          <label>Template</label>
+          <div>
+            <select value={template} onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+              setTemplate(e.target.value);
+            }}>{templates.map(data => (
+              <option key={data} value={data}>{data}</option>
+            ))}
+            </select>
           </div>
         </div>
       </div>
-    );
-  }
+      <div className='container'>
+        <div className='requirement-block'>
+          <label>Requiremenet</label>
+          <textarea value={requirement} rows={30}
+            onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
+              setRequirement(e.target.value);
+            }} />
+        </div>
+        <div className='container-column'>
+          <button onClick={handleGenerateStream}>=&gt;</button>
+        </div>
+        <div className='generated-block'>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <label>Generated Code</label>
+            <div>
+              {copied && <label className='success'>Copied</label>}
+              <button onClick={async (_e: MouseEvent) => {
+                navigator.clipboard.writeText(generated);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }} style={{ 'textAlign': 'right' }}>Copy</button>
+            </div>
+          </div>
+          <MarkdownViewer content={generated} height={426} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- frontend/src/components/Setting.tsx
+++ frontend/src/components/Setting.tsx
@@ -1,4 +1,4 @@
-import { type ChangeEvent, Component, type MouseEvent } from 'react';
+import { useEffect, useState, type ChangeEvent, type MouseEvent } from 'react';
 import { Link } from 'react-router-dom';
 import {
   fetchApiConfig,
@@ -16,150 +16,117 @@ import {
 import './Common.css';
 import './Setting.css';
 
-interface SettingState {
-  beBaseUrl: string;
-  apiSpecs: string[];
-  apiSpec: string;
-  baseUrl: string;
-  apiKey: string;
-  models: string[];
-  model: string;
-  tplDir: string;
-  templates: string[];
-  template: string;
-  content: string;
-}
+function Setting() {
+  const [beBaseUrl, setBeBaseUrlState] = useState(getBeBaseUrl());
+  const [apiSpecs, setApiSpecs] = useState<string[]>([]);
+  const [apiSpec, setApiSpec] = useState('');
+  const [baseUrl, setBaseUrl] = useState('');
+  const [apiKey, setApiKey] = useState('');
+  const [models, setModels] = useState<string[]>([]);
+  const [model, setModel] = useState('');
+  const [tplDir, setTplDir] = useState('');
+  const [templates, setTemplates] = useState<string[]>([]);
+  const [template, setTemplate] = useState('');
+  const [content, setContent] = useState('');
 
-class Setting extends Component<{}, SettingState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      beBaseUrl: getBeBaseUrl(),
-      apiSpecs: [],
-      apiSpec: '',
-      baseUrl: '',
-      apiKey: '',
-      model: '',
-      models: [],
-      tplDir: '',
-      templates: [],
-      template: '',
-      content: '',
-    };
+  function initSetting() {
+    initApiSpecs();
+    initModels();
+    initTemplates();
+    initConfig();
   }
 
-  componentDidMount() {
-    this.initSetting();
-  }
-
-  initSetting() {
-    this.initApiSpecs();
-    this.initModels();
-    this.initTemplates();
-    this.initConfig();
-  }
-
-  handleSaveBeBaseUrl = async (_e: MouseEvent) => {
-    const { beBaseUrl } = this.state;
+  const handleSaveBeBaseUrl = async (_e: MouseEvent) => {
     setBeBaseUrl(beBaseUrl);
-    this.initSetting();
+    initSetting();
   };
 
-  handleDetectBeBaseUrl = async (_e: MouseEvent) => {
+  const handleDetectBeBaseUrl = async (_e: MouseEvent) => {
     const protocol = window.location.protocol;
     const host = window.location.host;
     const url = `${protocol}//${host}`;
-    this.setState({ beBaseUrl: url })
+    setBeBaseUrlState(url);
   };
 
-  initApiSpecs() {
+  function initApiSpecs() {
     fetchApiSpecs().then((apiSpecs) => {
-      this.setState({ apiSpecs: apiSpecs });
+      setApiSpecs(apiSpecs);
     });
   }
 
-  initModels() {
-    fetchModels(false).then((models) => {
-      const { model } = this.state;
-      if (!models.includes(model)) {
-        models.push(model);
+  function initModels() {
+    fetchModels(false).then((modelsData) => {
+      if (!modelsData.includes(model)) {
+        modelsData.push(model);
       }
-      this.setState({ models: models });
+      setModels([...modelsData]);
     });
   }
 
-  handleReloadModels = async (_e: MouseEvent) => {
-    fetchModels(true).then((models) => {
-      const { model } = this.state;
-      if (!models.includes(model)) {
-        models.push(model);
+  const handleReloadModels = async (_e: MouseEvent) => {
+    fetchModels(true).then((modelsData) => {
+      if (!modelsData.includes(model)) {
+        modelsData.push(model);
       }
-      this.setState({ models: models });
+      setModels([...modelsData]);
     });
   };
 
-  initConfig() {
+  function initConfig() {
     fetchConfig().then(config => {
-      this.setState({
-        apiSpec: config.api_spec,
-        tplDir: config.tpl_dir,
-      });
-      this.reloadApiConfig(config.api_spec);
+      setApiSpec(config.api_spec);
+      setTplDir(config.tpl_dir);
+      reloadApiConfig(config.api_spec);
     });
   }
 
-  initTemplates() {
-    fetchTemplates(false).then(templates => {
-      this.setState({ templates: templates, template: templates[0] });
+  function initTemplates() {
+    fetchTemplates(false).then(templatesData => {
+      setTemplates(templatesData);
+      setTemplate(templatesData[0]);
     });
   }
 
-  reloadApiConfig(apiSpec: string) {
+  function reloadApiConfig(apiSpec: string) {
     fetchApiConfig(apiSpec).then((config) => {
-      console.log(config);
-      this.setState({
-        baseUrl: config.base_url || "",
-        apiKey: config.api_key || "",
-        model: config.model,
-      });
-    })
+      setBaseUrl(config.base_url || "");
+      setApiKey(config.api_key || "");
+      setModel(config.model);
+    });
   }
 
-  handelReloadTemplates = async (_e: MouseEvent) => {
-    fetchTemplates(true).then(templates => {
-      this.setState({ templates: templates, template: templates[0] });
+  const handelReloadTemplates = async (_e: MouseEvent) => {
+    fetchTemplates(true).then(templatesData => {
+      setTemplates(templatesData);
+      setTemplate(templatesData[0]);
     });
   };
 
-  handleLoadTemplate = async (_e: MouseEvent) => {
-    const { template } = this.state;
-    fetchTemplate(template).then(content => {
-      this.setState({ content: content });
+  const handleLoadTemplate = async (_e: MouseEvent) => {
+    fetchTemplate(template).then(contentData => {
+      setContent(contentData);
     });
   };
 
-  handleSaveTemplate = async (_e: MouseEvent) => {
-    const { template, content } = this.state;
+  const handleSaveTemplate = async (_e: MouseEvent) => {
     updateTemplate(template, content).then(() => {
       alert('Saved!');
-    })
+    });
   };
 
-  handleSaveConfig = async (_e: MouseEvent) => {
-    const { apiSpec, tplDir } = this.state;
+  const handleSaveConfig = async (_e: MouseEvent) => {
     const config = {
       'api_spec': apiSpec,
       'tpl_dir': tplDir,
     };
     updateConfig(JSON.stringify(config)).then(() => {
       alert('Setting Saved!');
-      this.initModels();
-      this.reloadApiConfig(apiSpec);
-    })
+      initModels();
+      reloadApiConfig(apiSpec);
+    });
   };
 
-  handleSaveApiConfig = async (_e: MouseEvent) => {
-    const { apiSpec, baseUrl, apiKey, model } = this.state
+  const handleSaveApiConfig = async (_e: MouseEvent) => {
     const config = {
       'base_url': baseUrl,
       'api_key': apiKey,
@@ -167,136 +134,134 @@ class Setting extends Component<{}, SettingState> {
     };
     updateApiConfig(apiSpec, JSON.stringify(config)).then(() => {
       alert('API Config Saved!');
-    })
+    });
   };
 
-  render() {
-    const {
-      beBaseUrl, apiSpecs, apiSpec, baseUrl, apiKey, models, model, tplDir, templates, template, content
-    } = this.state;
+  useEffect(() => {
+    initSetting();
+  }, []);
 
-    return (
-      <div className='main-frame'>
-        <div className='header'>
-          <Link to='/'>Return Home</Link>
-        </div>
-        <h1 className='title'>Settings</h1>
-        <label className='title'>General</label>
-        <div className='setting-container'>
-          <div className='setting'>
-            <div>
-              <label className='config-lable'>Backend Base URL: </label>
-              <input
-                type='text'
-                value={beBaseUrl}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                  this.setState({ beBaseUrl: e.target.value });
-                }}
-              />
-              <button onClick={this.handleSaveBeBaseUrl}>Save</button>
-              <button onClick={this.handleDetectBeBaseUrl}>Detect</button>
-            </div>
-          </div>
-        </div>
-        <label className='title'>Backend</label>
-        <div className='setting-container'>
-          <div className='setting'>
-            <div>
-              <label className='config-lable'>API Spec: </label>
-              <select value={apiSpec} onChange={(e: ChangeEvent<HTMLSelectElement>) => {
-                this.setState({ apiSpec: e.target.value })
-              }}>{apiSpecs.map(apiSpec => (
-                <option key={apiSpec} value={apiSpec}>{apiSpec}</option>
-              ))}
-              </select>
-            </div>
-          </div>
-          <div className='setting'>
-            <div>
-              <label className='config-lable'>Template Dir: </label>
-              <input
-                type='text'
-                value={tplDir}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                  this.setState({ tplDir: e.target.value });
-                }}
-              />
-            </div>
-          </div>
-          <div className='setting'>
-            <div>
-              <button onClick={this.handleSaveConfig}>Save</button>
-            </div>
-          </div>
-        </div>
-        <label className='title'>API Config</label>
-        <div className='setting-container'>
-          <div className='setting'>
-            <div>
-              <label className='config-lable'>API Base URL: </label>
-              <input
-                type='text'
-                value={baseUrl}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                  this.setState({ baseUrl: e.target.value });
-                }}
-                style={{ width: 240 }}
-              />
-            </div>
-          </div>
-          <div className='setting'>
-            <div>
-              <label className='config-lable'>API Key: </label>
-              <input
-                type='text'
-                value={apiKey}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                  this.setState({ apiKey: e.target.value });
-                }}
-                style={{ width: 280 }}
-              />
-            </div>
-          </div>
-          <div className='setting'>
-            <div>
-              <label className='config-lable'>Model: </label>
-              <select value={model} onChange={(e: ChangeEvent<HTMLSelectElement>) => {
-                this.setState({ model: e.target.value })
-              }}>{models.map(model => (
-                <option key={model} value={model}>{model}</option>
-              ))}
-              </select>
-              <button onClick={this.handleReloadModels}>Reload Models</button>
-            </div>
-          </div>
-          <div className='setting'>
-            <div>
-              <button onClick={this.handleSaveApiConfig}>Save</button>
-            </div>
-          </div>
-        </div>
-        <label className='title'>Template</label>
-        <div className='setting-container'>
+  return (
+    <div className='main-frame'>
+      <div className='header'>
+        <Link to='/'>Return Home</Link>
+      </div>
+      <h1 className='title'>Settings</h1>
+      <label className='title'>General</label>
+      <div className='setting-container'>
+        <div className='setting'>
           <div>
-            <button onClick={this.handelReloadTemplates}>Reload</button>
-            <label className='config-lable'>Template: </label>
-            <select value={template} onChange={(e: ChangeEvent<HTMLSelectElement>) => {
-              this.setState({ template: e.target.value })
-            }}>{templates.map(template => (
-              <option key={template} value={template}>{template}</option>
-            ))}
-            </select>
-            <button onClick={this.handleLoadTemplate}>Load</button>
-            <button onClick={this.handleSaveTemplate}>Save</button>
+            <label className='config-lable'>Backend Base URL: </label>
+            <input
+              type='text'
+              value={beBaseUrl}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setBeBaseUrlState(e.target.value);
+              }}
+            />
+            <button onClick={handleSaveBeBaseUrl}>Save</button>
+            <button onClick={handleDetectBeBaseUrl}>Detect</button>
           </div>
-          <textarea value={content} rows={20}
-            onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
-              this.setState({ content: e.target.value });
-            }} />
         </div>
       </div>
-    );
-  }
+      <label className='title'>Backend</label>
+      <div className='setting-container'>
+        <div className='setting'>
+          <div>
+            <label className='config-lable'>API Spec: </label>
+            <select value={apiSpec} onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+              setApiSpec(e.target.value);
+            }}>{apiSpecs.map(apiSpec => (
+              <option key={apiSpec} value={apiSpec}>{apiSpec}</option>
+            ))}
+            </select>
+          </div>
+        </div>
+        <div className='setting'>
+          <div>
+            <label className='config-lable'>Template Dir: </label>
+            <input
+              type='text'
+              value={tplDir}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setTplDir(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+        <div className='setting'>
+          <div>
+            <button onClick={handleSaveConfig}>Save</button>
+          </div>
+        </div>
+      </div>
+      <label className='title'>API Config</label>
+      <div className='setting-container'>
+        <div className='setting'>
+          <div>
+            <label className='config-lable'>API Base URL: </label>
+            <input
+              type='text'
+              value={baseUrl}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setBaseUrl(e.target.value);
+              }}
+              style={{ width: 240 }}
+            />
+          </div>
+        </div>
+        <div className='setting'>
+          <div>
+            <label className='config-lable'>API Key: </label>
+            <input
+              type='text'
+              value={apiKey}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setApiKey(e.target.value);
+              }}
+              style={{ width: 280 }}
+            />
+          </div>
+        </div>
+        <div className='setting'>
+          <div>
+            <label className='config-lable'>Model: </label>
+            <select value={model} onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+              setModel(e.target.value);
+            }}>{models.map(model => (
+              <option key={model} value={model}>{model}</option>
+            ))}
+            </select>
+            <button onClick={handleReloadModels}>Reload Models</button>
+          </div>
+        </div>
+        <div className='setting'>
+          <div>
+            <button onClick={handleSaveApiConfig}>Save</button>
+          </div>
+        </div>
+      </div>
+      <label className='title'>Template</label>
+      <div className='setting-container'>
+        <div>
+          <button onClick={handelReloadTemplates}>Reload</button>
+          <label className='config-lable'>Template: </label>
+          <select value={template} onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+            setTemplate(e.target.value);
+          }}>{templates.map(template => (
+            <option key={template} value={template}>{template}</option>
+          ))}
+          </select>
+          <button onClick={handleLoadTemplate}>Load</button>
+          <button onClick={handleSaveTemplate}>Save</button>
+        </div>
+        <textarea value={content} rows={20}
+          onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
+            setContent(e.target.value);
+          }} />
+      </div>
+    </div>
+  );
 }
 
 export default Setting;
